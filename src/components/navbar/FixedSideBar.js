728905f7f9c790c97e3fa00a89cfb0d915670ecb
c_lumapay <clumapay.info@gmail.com>
2021-06-30 06:20:48 +0800

    resume added

--- src/components/navbar/FixedSideBar.js
+++ src/components/navbar/FixedSideBar.js
@@ -3,7 +3,7 @@ import fixedLine from '../../images/fixed-line.png';
 import linkedin from '../../images/linkedin.png';
 import twitter from '../../images/twitter.png';
 import github from '../../images/github.png';
-
+import myResume from '../../utilities/myResume.pdf';
 
 const FixedSideBar = () => {
     return (
@@ -13,21 +13,21 @@ const FixedSideBar = () => {
                 <img className="fixed-left-line" src={fixedLine} alt="" />
                 <div className="fixed-left-sm">
                     <div>
-                    <a href="https://www.linkedin.com/in/christian-virtual-assistant/" target="_blank"><img className="my-4 linkedin" src={linkedin} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/christian-virtual-assistant/" target="_blank"><img className="my-4 linkedin" src={linkedin} alt="" /></a>
                     </div>
                     <div>
-                    <a href="https://twitter.com/c_lumapay30" target="_blank"><img className="my-4 twitter" src={twitter} alt="" /></a>
+                        <a href="https://twitter.com/c_lumapay30" target="_blank"><img className="my-4 twitter" src={twitter} alt="" /></a>
                     </div>
                     <div>
-                    <a href="https://github.com/clumapay30" target="_blank"><img className="my-4 github" src={github} alt="" /></a>
+                        <a href="https://github.com/clumapay30" target="_blank"><img className="my-4 github" src={github} alt="" /></a>
                     </div>
                     <div>
-                        <button className="resume-btn">Resume</button>
+                        <a href={myResume} target="_blank"><button className="resume-btn">Resume</button></a>
+                    </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+            )
 }
 
-export default FixedSideBar;
+            export default FixedSideBar;
